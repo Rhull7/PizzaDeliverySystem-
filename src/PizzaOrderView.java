d934895611e19ae7faa832edbea5c796379d603a
kieblade <khfan33@Yahoo.com>
2014-12-04 12:27:21 -0700

    Update PizzaOrderView.java

--- src/PizzaOrderView.java
+++ src/PizzaOrderView.java
@@ -1,185 +1,190 @@
-package pds;
+package Pizza;
 
+//Image by suphakit73 on http://www.freedigitalphotos.net
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-//import java.awt.Image;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 @SuppressWarnings("serial")
-public class PizzaOrderView extends JFrame {
-	Customer customer= new Customer();
-	public PizzaOrderView() {
+public class PizzaOrderView extends JFrame
+{
+	PizzaOrderModel Pizza = new PizzaOrderModel();
+	JTextField phoneField;
+	JPanel namePanel;
+	JLabel nameLabel;
+	JTextField nameField;
+	JLabel addressLabel;
+	JTextField addressField;
+	JLabel instructionsLabel;
+	JTextField instructionsField;
+	JButton update;
+	JButton phoneButton;
+	JCheckBox An;
+	JCheckBox Pep;
+	JCheckBox S;
+	JCheckBox It;
+	JCheckBox Black;
+	JCheckBox M;
+	JCheckBox On;
+	JCheckBox RP;
+	JRadioButton Small;
+	JRadioButton Med;
+	JRadioButton Large;
+	JButton addToOrderButton;
+	JButton totalTheOrderButton;
+	JTextField orderSummaryField;
+	JScrollPane Scroll;
+	JButton submit;
+	JButton clear;
+	JTextField instructionsField1;
+	JScrollPane Scroll2;
+	
+	public PizzaOrderView()
+	{
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setTitle("Guido's Pizza");
 		
-
+		
 		Border raisedBorder = BorderFactory.createEmptyBorder(0, 2, 2, 2);
 		Font font = new Font("Verdana", Font.ITALIC, 50);
-		JPanel contentPane = new JPanel() {
-			public void paintComponent(Graphics g) {
-				// Image img =
-				// Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("ID-100213720.jpg"));
-				// g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(),
-				// this);
-			}
-		};
+		JPanel contentPane = new JPanel() 
+		 {  
+			 	public void paintComponent(Graphics g) 
+			{  
+			 	Image img = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("ID-100213720.jpg"));  
+		 		g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+		 	}
+		 };  
 		contentPane.setPreferredSize(new Dimension(400, 900));
-		Border Raised = BorderFactory.createTitledBorder(raisedBorder,
-				"Guido's Pizzeria", TitledBorder.TOP, TitledBorder.CENTER,
-				font, Color.RED);
+		Border Raised = BorderFactory.createTitledBorder(raisedBorder, "Guido's Pizzeria", TitledBorder.TOP, TitledBorder.CENTER, font, Color.RED);
 		contentPane.setBorder(Raised);
 		setContentPane(contentPane);
 		ButtonGroup B1 = new ButtonGroup();
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		GridLayout G = new GridLayout(1, 2, 10, 10);
-
+		
 		JPanel customerInfoPanel = new JPanel();
 		customerInfoPanel.setPreferredSize(new Dimension(400, 10));
-		customerInfoPanel.setBorder(BorderFactory
-				.createTitledBorder("Customer Details"));
+		customerInfoPanel.setBorder(BorderFactory.createTitledBorder("Customer Details"));
 		BoxLayout B = new BoxLayout(customerInfoPanel, BoxLayout.Y_AXIS);
 		customerInfoPanel.setLayout(B);
-
-		JPanel namePanel = new JPanel();
+		
+		namePanel = new JPanel();
 		namePanel.setLayout(G);
-		JLabel nameLabel = new JLabel("name");
+		nameLabel = new JLabel("name");
 		nameLabel.setOpaque(true);
 		nameLabel.setBackground(Color.ORANGE);
 		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		nameLabel.setVerticalAlignment(SwingConstants.CENTER);
-		final JTextField nameField = new JTextField();
+		nameField = new JTextField();
 		namePanel.add(nameLabel);
 		namePanel.add(nameField);
 		namePanel.setOpaque(false);
 		customerInfoPanel.add(namePanel);
 		JPanel addressPanel = new JPanel();
 		addressPanel.setLayout(new GridLayout(1, 2, 10, 10));
-		JLabel addressLabel = new JLabel("address");
+		addressLabel = new JLabel("address");
 		addressLabel.setOpaque(true);
 		addressLabel.setBackground(Color.ORANGE);
 		addressLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		addressLabel.setVerticalAlignment(SwingConstants.CENTER);
-		final JTextField addressField = new JTextField();
+		addressField = new JTextField();
 		addressPanel.add(addressLabel);
 		addressPanel.add(addressField);
 		addressPanel.setOpaque(false);
 		customerInfoPanel.add(addressPanel);
-
+		
 		JPanel instructionsPanel = new JPanel();
 		instructionsPanel.setLayout(new GridLayout(1, 2, 10, 10));
-		JLabel instructionsLabel = new JLabel("instructions");
+		instructionsLabel = new JLabel("instructions");
 		instructionsLabel.setOpaque(true);
 		instructionsLabel.setBackground(Color.ORANGE);
 		instructionsLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		instructionsLabel.setVerticalAlignment(SwingConstants.CENTER);
-		final JTextField instructionsField = new JTextField();
+		instructionsField = new JTextField();
 		instructionsPanel.add(instructionsLabel);
 		instructionsPanel.add(instructionsField);
 		instructionsPanel.setOpaque(false);
 		customerInfoPanel.add(instructionsPanel);
-
-		JButton update = new JButton("Update Customer Info");
-		update.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				
-				customer.setName(nameField.toString());
-				customer.setName(addressField.toString());
-				customer.setName(instructionsField.toString());
-			}
-		});
+		
+		update = new JButton("Update Customer Info");
 		update.setAlignmentX(CENTER_ALIGNMENT);
+		update.setEnabled(false);
+		update.addActionListener(new PizzaOrderView.UpdateListener());
 		customerInfoPanel.add(update);
 		customerInfoPanel.setOpaque(false);
 		customerInfoPanel.setPreferredSize(new Dimension(60, 5));
 		getContentPane().add(customerInfoPanel);
 		
-		
-		
 		JPanel phonePanel = new JPanel();
 		phonePanel.setPreferredSize(new Dimension(400, 20));
-		phonePanel.setBorder(BorderFactory
-				.createTitledBorder("Customer Phone Number"));
-		JButton phoneButton = new JButton("Search phone #");
-		
-		final JTextField phoneField = new JTextField();
+		phonePanel.setBorder(BorderFactory.createTitledBorder("Customer Phone Number"));
+		phoneButton = new JButton("Search phone #");
+		phoneButton.addActionListener(new PizzaOrderView.PhoneListener());
+		phoneField = new JTextField();
 		phonePanel.setLayout(new GridLayout(0, 2, 0, 0));
 		phonePanel.add(phoneButton);
 		phonePanel.add(phoneField);
 		phonePanel.setOpaque(false);
 		getContentPane().add(phonePanel);
-		phoneButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				PizzaOrderModel model = new PizzaOrderModel();
-				model.find(phoneField.toString());
-				customer.setPhoneNumber(phoneField.toString());
-			}
-		});
 		
-
 		JPanel orderPanel = new JPanel();
 		orderPanel.setPreferredSize(new Dimension(400, 100));
 		orderPanel.setBorder(BorderFactory.createTitledBorder("Pizza Orders"));
 		orderPanel.setLayout(new BoxLayout(orderPanel, BoxLayout.Y_AXIS));
-
+		
 		JPanel toppingsPanel = new JPanel();
 		toppingsPanel.setLayout(new BoxLayout(toppingsPanel, BoxLayout.X_AXIS));
-
+		
 		JPanel meatToppingsPanel = new JPanel();
 		meatToppingsPanel.setBackground(Color.ORANGE);
 		meatToppingsPanel.setLayout(new GridLayout(4, 1));
-		meatToppingsPanel.setBorder(BorderFactory
-				.createTitledBorder("Meat Toppings:"));
-		JCheckBox An = new JCheckBox("Anchovies");
+		meatToppingsPanel.setBorder(BorderFactory.createTitledBorder("Meat Toppings:"));
+		An = new JCheckBox("Anchovies");
 		An.setOpaque(false);
-		JCheckBox Pep = new JCheckBox("Pepperoni");
+		Pep = new JCheckBox("Pepperoni");
 		Pep.setOpaque(false);
-		JCheckBox S = new JCheckBox("Sausage");
+		S = new JCheckBox("Sausage");
 		S.setOpaque(false);
-		JCheckBox It = new JCheckBox("Italian Sausage");
+		It = new JCheckBox("Italian Sausage");
 		It.setOpaque(false);
 		meatToppingsPanel.add(An);
 		meatToppingsPanel.add(Pep);
 		meatToppingsPanel.add(S);
 		meatToppingsPanel.add(It);
 		toppingsPanel.add(meatToppingsPanel);
-
+		
 		JPanel veggieToppingsPanel = new JPanel();
 		veggieToppingsPanel.setBackground(Color.ORANGE);
 		veggieToppingsPanel.setLayout(new GridLayout(4, 1));
-		veggieToppingsPanel.setBorder(BorderFactory
-				.createTitledBorder("Veggie Toppings:"));
-		JCheckBox Black = new JCheckBox("Black Olives");
+		veggieToppingsPanel.setBorder(BorderFactory.createTitledBorder("Veggie Toppings:"));
+		Black = new JCheckBox("Black Olives");
 		Black.setOpaque(false);
-		JCheckBox M = new JCheckBox("Mushrooms");
+		M = new JCheckBox("Mushrooms");
 		M.setOpaque(false);
-		JCheckBox On = new JCheckBox("Onions");
+		On = new JCheckBox("Onions");
 		On.setOpaque(false);
-		JCheckBox RP = new JCheckBox("Roasted Peppers");
+		RP = new JCheckBox("Roasted Peppers");
 		RP.setOpaque(false);
 		veggieToppingsPanel.add(Black);
 		veggieToppingsPanel.add(M);
@@ -189,17 +194,16 @@ public class PizzaOrderView extends JFrame {
 		orderPanel.setOpaque(false);
 		toppingsPanel.setOpaque(false);
 		orderPanel.add(toppingsPanel);
-
+		
 		JPanel pizzaSizesPanel = new JPanel();
 		pizzaSizesPanel.setBackground(Color.ORANGE);
-		pizzaSizesPanel.setBorder(BorderFactory
-				.createTitledBorder("Pizza sizes:"));
+		pizzaSizesPanel.setBorder(BorderFactory.createTitledBorder("Pizza sizes:"));
 		pizzaSizesPanel.setLayout(new FlowLayout(1, 3, 1));
-		JRadioButton Small = new JRadioButton("Small");
+		Small = new JRadioButton("Small");
 		Small.setOpaque(false);
-		JRadioButton Med = new JRadioButton("Medium");
+		Med = new JRadioButton("Medium");
 		Med.setOpaque(false);
-		JRadioButton Large = new JRadioButton("Large");
+		Large = new JRadioButton("Large");
 		Large.setSelected(true);
 		Large.setOpaque(false);
 		B1.add(Small);
@@ -209,115 +213,192 @@ public class PizzaOrderView extends JFrame {
 		pizzaSizesPanel.add(Med);
 		pizzaSizesPanel.add(Large);
 		orderPanel.add(pizzaSizesPanel);
-
+		
 		JPanel orderButtonsPanel = new JPanel();
 		orderButtonsPanel.setLayout(new GridLayout(1, 2, 10, 10));
-		JButton addToOrderButton = new JButton("Add pizza to this order");
-		addToOrderButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
+		addToOrderButton = new JButton("Add pizza to this order");
+		addToOrderButton.setEnabled(false);
 		orderButtonsPanel.add(addToOrderButton);
-		JButton totalTheOrderButton = new JButton("Total this order");
-		totalTheOrderButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
+		totalTheOrderButton = new JButton("Total this order");
+		totalTheOrderButton.setEnabled(false);
 		orderButtonsPanel.add(totalTheOrderButton);
 		orderButtonsPanel.setOpaque(false);
 		orderPanel.add(orderButtonsPanel);
 		getContentPane().add(orderPanel);
-
+		
 		JPanel orderSummaryPanel = new JPanel();
 		orderSummaryPanel.setPreferredSize(new Dimension(400, 60));
-		orderSummaryPanel.setBorder(BorderFactory
-				.createTitledBorder("Order Summary:"));
+		orderSummaryPanel.setBorder(BorderFactory.createTitledBorder("Order Summary:"));
 		orderSummaryPanel.setLayout(new FlowLayout(1, 1, 1));
-		JTextField orderSummaryField = new JTextField();
+		orderSummaryField = new JTextField();
 		orderSummaryField.setPreferredSize(new Dimension(1000, 40));
-		JScrollPane Scroll = new JScrollPane(orderSummaryField);
+		Scroll = new JScrollPane(orderSummaryField);
 		Scroll.setPreferredSize(new Dimension(300, 70));
 		Scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		orderSummaryPanel.setOpaque(false);
 		orderSummaryPanel.add(Scroll);
-		getContentPane().add(orderSummaryPanel);
-
+		add(orderSummaryPanel);
+		
+		
 		JPanel finishUpPanel = new JPanel();
 		finishUpPanel.setPreferredSize(new Dimension(400, 20));
 		finishUpPanel.setLayout(new GridLayout(1, 2, 10, 10));
-		finishUpPanel.setBorder(BorderFactory
-				.createTitledBorder("Finish the Order:"));
-		JButton submit = new JButton("Submit");
-		JButton clear = new JButton("Clear");
+		finishUpPanel.setBorder(BorderFactory.createTitledBorder("Finish the Order:"));
+		submit = new JButton("Submit");
+		submit.setEnabled(false);
+		clear = new JButton("Clear");
+		clear.addActionListener(new ClearListener());
 		finishUpPanel.add(submit);
 		finishUpPanel.add(clear);
 		finishUpPanel.setOpaque(false);
 		getContentPane().add(finishUpPanel);
-
+		
 		JPanel instructionsPanel1 = new JPanel();
 		instructionsPanel1.setPreferredSize(new Dimension(400, 40));
-		instructionsPanel1.setBorder(BorderFactory
-				.createTitledBorder("Employee instructions:"));
+		instructionsPanel1.setBorder(BorderFactory.createTitledBorder("Employee instructions:"));
 		instructionsPanel1.setLayout(new FlowLayout(1, 1, 1));
-		JTextField instructionsField1 = new JTextField(
-				"Enter the customers phone number.");
-		instructionsField1.setPreferredSize(new Dimension(1000, 20));
-		;
-		JScrollPane Scroll2 = new JScrollPane(instructionsField1);
+		instructionsField1 = new JTextField("Enter the customers phone number.");
+		instructionsField1.setPreferredSize(new Dimension(1000, 20));;
+		Scroll2 = new JScrollPane(instructionsField1);
 		Scroll2.setPreferredSize(new Dimension(400, 40));
 		instructionsPanel1.add(Scroll2);
 		instructionsPanel1.setOpaque(false);
 		getContentPane().add(instructionsPanel1);
-
+		
 		pack();
+	}
+	
+	private class PhoneListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			String phoneN = phoneField.getText();
+			if(Pizza.find(phoneN) == null)
+			{
+				orderSummaryField.setText("Customer not found");
+				phoneButton.setEnabled(false);
+				update.setEnabled(true);
+			}
+			else
+			{
+				orderSummaryField.setText("Customer found");
+				update.setEnabled(true);
+				addToOrderButton.setEnabled(true);
+			}
+		}
+	}
+	
+	private class UpdateListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(nameField.getText().equals(""))
+			{
+				orderSummaryField.setText("You need to fill all the options in the update.");
+			}
+			else if(addressField.getText().equals(""))
+			{
+				orderSummaryField.setText("You need to fill all the options in the update.");
+			}
+			else if(instructionsField.getText().equals(""))
+			{
+				orderSummaryField.setText("You need to fill all the options in the update.");
+			}
+			else
+			{
+				String Name = nameField.getText();
+				String address = addressField.getText();
+				String instructions = instructionsField.getText();
+				String phoneN = phoneField.getText();
+				Customer N = new Customer(phoneN, Name, address, instructions);
+				Pizza.add(N);
+				Pizza.shutdown();
+				orderSummaryField.setText("");
+				
+				if(addToOrderButton.isEnabled() == false)
+				{
+					addToOrderButton.setEnabled(true);
+					
+				}
+				
+			}
+		}
+	}
+	
+	private class ClearListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			addToOrderButton.setEnabled(false);
+			totalTheOrderButton.setEnabled(false);
+			submit.setEnabled(false);
+			clear.setEnabled(false);
+			update.setEnabled(false);
+			phoneButton.setEnabled(true);
+		    phoneField.setText("");
+			nameField.setText("");
+			addressField.setText("");
+			instructionsField.setText("");
+			An.setSelected(false);
+			Pep.setSelected(false);
+			S.setSelected(false);
+			It.setSelected(false);
+			Black.setSelected(false);
+			M.setSelected(false);
+			On.setSelected(false);
+			RP.setSelected(false);
+			Small.setSelected(false);
+			Med.setSelected(false);
+			Large.setSelected(true);
+			orderSummaryField.setText("");
+			instructionsField1.setText("");
+		}
 	}
 
 	/**
 	 * Launch the application.
-	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-		PizzaOrderModel pizza = new PizzaOrderModel();
-		
+	public static void main(String[] args) 
+	{
 		PizzaOrderView P = new PizzaOrderView();
 		P.setSize(600, 700);
 		P.setVisible(true);
 	}
-
-	public int getScrollableUnitIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
+	
+	public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) 
+	{
 		int maxUnitIncrement = 1000;
-		// Get the current position.
+//Get the current position.
 		int currentPosition = 0;
-		if (orientation == SwingConstants.HORIZONTAL) {
+		if (orientation == SwingConstants.HORIZONTAL) 
+		{
 			currentPosition = visibleRect.x;
-		} else {
+		} 
+		else 
+		{
 			currentPosition = visibleRect.y;
 		}
 
-		// Return the number of pixels between currentPosition
-		// and the nearest tick mark in the indicated direction.
-		if (direction < 0) {
-			int newPosition = currentPosition
-					- (currentPosition / maxUnitIncrement) * maxUnitIncrement;
+//Return the number of pixels between currentPosition
+//and the nearest tick mark in the indicated direction.
+		if (direction < 0) 
+		{
+			int newPosition = currentPosition -(currentPosition / maxUnitIncrement)* maxUnitIncrement;
 			return (newPosition == 0) ? maxUnitIncrement : newPosition;
-		} else {
-			return ((currentPosition / maxUnitIncrement) + 1)
-					* maxUnitIncrement - currentPosition;
+		} 
+		else 
+		{
+			return ((currentPosition / maxUnitIncrement) + 1)* maxUnitIncrement- currentPosition;
 		}
 	}
-
-	public int getScrollableBlockIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
+	
+	public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) 
+	{
 		int maxUnitIncrement = 1000;
 		if (orientation == SwingConstants.HORIZONTAL)
 			return visibleRect.width - maxUnitIncrement;
 		else
 			return visibleRect.height - maxUnitIncrement;
 	}
-
 }
